c3e980ac07745c0d358d8d22fa4fc1852c195da0
Kreliannn <krelianquimson@gmail.com>
2025-01-16 14:28:49 +0800

    add run button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,6 +14,8 @@ export default function Home() {
         src="/pokemon_header.png" alt="no img"
         className='w-screen h-52 m-auto '  
       />
+
+      <button className=' rounded-full  text-lg  absolute bg-cover bg-center bg-no-repeat active:scale-125' style={{bottom : "7%", left : "-1%", padding : "14%", backgroundImage : "url('/run_button.png')"}}> </button>    
     </div>
   );
 }
